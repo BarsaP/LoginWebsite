2c6752aa5aba98d358ab8f13fac86da656d828f1
barsa <barshapriyadarshini0602@gmail.com>
2024-02-05 00:00:37 +0530

    final commit

---
+++ LoginWebsite/src/main/java/com/nt/pk/Login.java
@@ -0,0 +1,54 @@
+package com.nt.pk;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uemail = request.getParameter("username");
+		String upwd = request.getParameter("password");
+		HttpSession session = request.getSession();
+		RequestDispatcher dispatcher = null;
+		Connection con = null;
+		
+		try {
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+    		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/barsadb?useSSL=false","root","Varsa@22");
+    		PreparedStatement pst = con.prepareStatement("select * from users where uemail = ? and upwd = ?");
+    		pst.setString(1, uemail);
+    		pst.setString(2, upwd);
+    		
+    		ResultSet rs = pst.executeQuery();
+    		if(rs.next()) {
+    			session.setAttribute("name", rs.getString("uname"));
+    			dispatcher = request.getRequestDispatcher("index.jsp");
+    		}else {
+    			request.setAttribute("status", "failed");
+    			dispatcher = request.getRequestDispatcher("login.jsp");
+    		}
+    		dispatcher.forward(request, response);
+    		
+	}catch(Exception e){
+		e.printStackTrace();
+	  }
+	}
+
+}
